b375d62d797e96e596dea0b291122d9f265d733a
Guilherme <guilherme.hagah@gmail.com>
2025-09-18 16:53:47 -0300

    Add files via upload

---
+++ j1.js
@@ -0,0 +1,42 @@
+const prompt = require("prompt-sync")()
+
+let nomes = []
+let quartos = []
+let emails = []
+
+let qcd = Number(prompt("Quantos quartos serão alugados?: "))
+let quarto = ""
+
+for  (let i = 1; i < qcd+1; i++) {
+    console.log(i);
+    while (true){
+        quarto = Number(prompt(`Quarto do ${i}º ospedado: `))        
+        if (verificarQuarto())
+            break
+    }
+    console.log("");
+    quartos.push(quarto)
+
+    let nome = prompt(`Nome do ${i}º ospedado: `)
+    nomes.push(nome)
+    console.log("");
+
+    let email = prompt(`O email do ${i}º ospedado: `)
+    emails.push(email)
+    console.log("");
+}
+console.log("");
+for  (let i = 0; i < nomes.length; i++) {
+    console.log(`Quartos: ${quartos[i]} ,Nome: ${nomes[i]} ,Email: ${emails[i]}\n`);
+    
+}
+
+function verificarQuarto() {
+    for (let i = 0; i < quartos.length; i++) {
+        if (quartos[i] === quarto) {
+            console.log(`\x1b[31m[quarto ocupado, tente outro]\x1b[0m\n`)
+            return false      
+    }
+}
+return true
+}
